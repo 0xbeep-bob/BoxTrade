c10a63afe19beccca1d3fa77e586bfac6343e2e7
Evgeny <zheka.borodach@yandex.ru>
2020-01-20 21:28:11 +0300

    Add project files.

---
+++ BoxTrade/Models/OrderModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxTrade.Models
+{
+    public class OrderModel
+    {
+        public int duration { get; set; }
+        public bool is_buy_order { get; set; }
+        public DateTime issued { get; set; }
+        public object location_id { get; set; }
+        public int min_volume { get; set; }
+        public object order_id { get; set; }
+        public decimal price { get; set; }
+        public string range { get; set; }
+        public int system_id { get; set; }
+        public int type_id { get; set; }
+        public int volume_remain { get; set; }
+        public int volume_total { get; set; }
+    }
+}
---
+++ BoxTrade/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using BoxTrade.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxTrade.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        HttpClient client = new HttpClient();
+        string requeststring = "https://esi.evetech.net/latest/markets/10000002/orders/?type_id=34133";
+
+        public MainWindowViewModel()
+        {
+            CalculateAsync();
+
+
+        }
+
+        public async Task CalculateAsync()
+        {
+            var data = await GetDataAsync(requeststring);
+
+            var prices = JsonConvert.DeserializeObject<List<OrderModel>>(data);
+
+            var minPrice = prices.Where(x => x.is_buy_order = true && x.system_id == 30000142).Max(x => x.price);
+        }
+
+        public async Task<string> GetDataAsync(string source)
+        {
+            HttpResponseMessage response = await client.GetAsync(source);
+            string result = await response.Content.ReadAsStringAsync();
+
+            return result;
+        }
+    }
+}
